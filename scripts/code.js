a79dcabef39f7411e0c462cf90313a89afe54cb4
aescys <012jaes@gmail.com>
2025-09-28 00:45:39 -0400

    testing site scaling on mobile devices, removed <meta viewport> from index.html to see if it will now scale in the same manner as siteOneOffs/cliTutorial.html, also a patchwork content to code.html in the form of a homebrew install guide

--- scripts/code.js
+++ scripts/code.js
@@ -15,7 +15,7 @@ document.getElementById("1180Content").addEventListener('click', (event) => {
         </div>
         <h2>Installing Java's Toolkit</h2>
         <div id="java_install">
-            <p>WIP: Expect within 24 hours</p>
+            <p>You can find the guide for installing and putting to use the Java Development Kit <a href="siteOneOffs/code/jdkTutorial.html" target="_blank">here</a>. It is recommended that you are familiar with the command line or have completed the command line tutuorial shown above.</p>
         </div>
         <hr>
     </div>
@@ -32,6 +32,19 @@ document.getElementById("2200Content").addEventListener('click', (event) => {
 
 document.getElementById("programContent").addEventListener('click', (event) => {
     content.innerHTML = `
+                        <div id="prog2">
+                            <hr>
+                            <h1>Mac OS CLI Must Have</h1>
+                            <p>If you are a macOS user and you are looking to get into programming you must learn to use the command line interface, and furthermore you must install a package manager for your command line interface.</p>
+                            <h3>Homebrew is that package manager!</h3>
+                            <p>And also, lucky for you I have a basic tutorial for the command line interface already posted on this site.. Check out WSU CS 1180 Content on the left, to find a fully polished CLI tutorial.</p>
+                            <p>Go ahead and finish that up, and then come back here and run the command <a>/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"</a> in your terminal application</p>
+                            <p>You now have a package manager.</p>
+                            <p>For a cool starter package to see how capable the command line really is, try checking out yt-dlp listed on the footer of this website below</p>
+                            <p>Now that you have homebrew installed, you can just run the command, "brew install yt-dlp"</p>
+                            <p>Sidenote, you can verify this script yourself before installing it, just go to <a>https://brew.sh</a> and trust me when I tell you homebrew is <a id="italicize">the</a> package manager for macOS.</p>
+                            <hr>
+                        </div>
                         <div id="prog1">
                             <hr>
                             <h1>Mouse Tracker Program</h1>
